52a14c615d77045bd480258e0d2a335a11a6a096
Yu3Ga0SioMGames <yu3ga0siom@gmail.com>
2024-01-27 01:04:46 +0300

    Return back old metods;

--- C_Tutor/main.c
+++ C_Tutor/main.c
@@ -3,54 +3,61 @@
 #include <stdint.h>
 
 
+#define ND_CREATE_ERROR 1
+#define BAD_POS_ERROR (ND_CREATE_ERROR + 1)
+
+
 typedef
-struct _node{
+struct _node {
     int64_t data;
     struct _node *next;
 } Node;
 
 Node *create_node(int64_t);
-void *free_node(Node*);
 
-void print_list(Node*);
+void free_node(Node *);
 
-//void append_to_list();
+void print_list(Node *);
 
-//void insert_in_list();
+int insert_to_list(Node **, size_t, int64_t);
 
-//void prepend_to_list();
+int prepend_to_list(Node **, int64_t);
 
-//void delete_from_list();
+int delete_from_list(Node **, size_t);
 
-void add_to_list(Node**, int64_t);
+size_t list_length(Node *);
 
-int get_from_list(Node*, size_t, int64_t*);
+int append_to_list(Node **, int64_t);
 
-Node *get_node_from_list(Node*, size_t);
+int get_from_list(Node *, size_t, int64_t *);
 
-int main(){
+Node *get_node_from_list(Node *, size_t);
+
+int main() {
     Node *list = NULL;
-    for (int64_t i = 0; i < 10; ++i){
-        add_to_list(&list, i);
+    for(int64_t i = 0; i < 5; ++i){
+        prepend_to_list(&list, i);
     }
-    print_list(list);
-    int64_t value;
-    size_t n;
-    printf("\n");
-    scanf("%zd", &n);
-    int return_code = get_from_list(list, n, &value);
-    if (return_code == 0){
-        printf("\n%lld\n", value);
-    }
-    else{
-        printf("\nElement not found!\n");
-    }
+    print_list(list); // 4 3 2 1 0
+    delete_from_list(&list, 3);
+    print_list(list); // 4 3 2 0
+    delete_from_list(&list, 4);
+    print_list(list); // 4 3 2 0
+    delete_from_list(&list, 0);
+    print_list(list); // 3 2 0
+    delete_from_list(&list, 2);
+    print_list(list); // 3 2
+    delete_from_list(&list, 0);
+    delete_from_list(&list, 0);
+    print_list(list); //
+    delete_from_list(&list, 0);
+    print_list(list); //
     return 0;
 }
 
-Node *create_node(int64_t input_data){
-    Node *node = (Node*)malloc(sizeof(Node));
-    if (node == NULL){
+Node *create_node(int64_t input_data) {
+    Node *node = (Node *)malloc(sizeof(Node));
+    if(node == NULL) {
         return NULL;
     }
 
@@ -59,69 +66,112 @@ Node *create_node(int64_t input_data){
     return node;
 }
 
-void *free_node(Node* input_node){
+void free_node(Node *input_node) {
     free(input_node);
 }
 
-void add_to_list(Node** list, int64_t data){
+int append_to_list(Node **list, int64_t data) {
     Node *new_node = create_node(data);
-    if (new_node == NULL){
-        return;
+    if(new_node == NULL) {
+        return ND_CREATE_ERROR;
     }
 
-    Node* next_node = *list;
-    if (next_node != NULL){
-        while (next_node->next != NULL){
+    Node *next_node = *list;
+    if(next_node != NULL) {
+        while (next_node->next != NULL) {
             next_node = next_node->next;
         }
         next_node->next = new_node;
     }
-    else{
+    else {
         *list = new_node;
     }
+    return 0;
 }
 
-void print_list(Node* list){
-    while (list != NULL){
-        printf("%lld, ", list->data);
+void print_list(Node *list) {
+    while(list != NULL) {
+        printf("%lld ", list->data);
 
         list = list->next;
     }
     printf("\n");
 }
 
-//Получает ноду - структуру из списка по заданному номеру.
-Node *get_node_from_list(Node* current_node, size_t index){
-    for (; index != 0 && current_node != NULL; --index){
+Node *get_node_from_list(Node *current_node, size_t index) {
+    for(; index != 0 && current_node != NULL; --index) {
         current_node = current_node->next;
     }
     return current_node;
 }
 
-//Получает данные из списка по заданному номеру. Эквивалентно: list[index]
-int get_from_list(Node *list, size_t index, int64_t *return_value){
+int get_from_list(Node *list, size_t index, int64_t *return_value) {
     Node *node_ptr = get_node_from_list(list, index);
-    if (node_ptr != NULL){
+    if(node_ptr != NULL) {
         *return_value = node_ptr->data;
         return 0;
     }
-    else{
+    else {
         return 1;
     }
 }
 
-/*void append_to_list(){
-
+int insert_to_list(Node **list, size_t index, int64_t data) {
+    if(index == 0) {
+        return prepend_to_list(list, data);
+    }
+    Node *new_node = create_node(data);
+    if(new_node == NULL) {
+        return ND_CREATE_ERROR;
+    }
+    Node *prev_node = get_node_from_list(*list, index - 1);
+    if(prev_node == NULL) {
+        return BAD_POS_ERROR;
+    }
+    new_node->next = prev_node->next;
+    prev_node->next = new_node;
+    return 0;
 }
 
-void insert_in_list(){
-
+int prepend_to_list(Node **list, int64_t data) {
+    Node *new_node = create_node(data);
+    if(new_node == NULL) {
+        return ND_CREATE_ERROR;
+    }
+    new_node->next = *list;
+    *list = new_node;
+    return 0;
 }
 
-void prepend_to_list(){
-
+int delete_from_list(Node **list, size_t index) {
+    if(*list == NULL) {
+        return BAD_POS_ERROR;
+    }
+    Node *current_node;
+    if(index == 0) {
+        current_node = *list;
+        *list = current_node->next;
+    }
+    else {
+        Node *prev_node = get_node_from_list(*list, index - 1);
+        if(prev_node == NULL) {
+            return BAD_POS_ERROR;
+        }
+        current_node = prev_node->next;
+        if(current_node == NULL) {
+            return BAD_POS_ERROR;
+        }
+        prev_node->next = current_node->next;
+    }
+    free_node(current_node);
+    return 0;
 }
 
-void delete_from_list(){
-
-}*/
+size_t list_length(Node *list) {
+    size_t count = 0;
+    while(list == NULL) {
+        list = list->next;
+        ++count;
+    }
+    return count;
+}
