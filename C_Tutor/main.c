4478d9e4e7f6c2db2e1f2c6d3f6c89d0bc9474c4
Yu3Ga0SioMGames <yu3ga0siom@gmail.com>
2024-02-11 22:06:45 +0300

    Update main.c;

--- C_Tutor/main.c
+++ C_Tutor/main.c
@@ -4,5 +4,16 @@
 
 
 int main() {
+    Array *array;
+    size_t initial_size;
+    printf("Введите размер массива: ");
+    scanf("%zu", &initial_size);
+    array = create_array(initial_size);
+
+    for (int i = 0; i < initial_size; ++i) {
+        append_to_array(array, i);
+    }
+
+    print_array(array);
     return 0;
 }
